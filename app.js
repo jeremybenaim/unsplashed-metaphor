c60ea58990b694ae822289158c9bddebd33b1225
Jeremy Benaim <jeremybenaim@gmail.com>
2013-07-26 15:42:31 -0700

    enabled compression – fixed accidental commit

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ http.createServer(function (req, res) {
         });
       } else {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        buf.pipe(res);
+        res.end(markup);
       }
 
     });
